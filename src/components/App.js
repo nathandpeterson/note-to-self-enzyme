b62c5ae2e19bc0e7ed2156c7c4508070bfd1393d
Nathan Daniel Peterson <natperson@gmail.com>
2018-03-05 16:34:15 -0800

    add basic input form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
-
+import { Button, Form, FormControl } from 'react-bootstrap'
 class App extends Component {
+
+    state = {text: ''}
+
+    handleChange = (e) => {
+        this.setState({text: e.target.value})
+    }
+
+    handleClick = () => {
+        this.setState({text: ''})        
+    }
 
     render() {
         return (
-            <h2>Note to Self</h2>
+            <div>
+            <h2>NOTE TO SELF</h2>
+            <Form inline>
+                <FormControl onChange={this.handleChange} />
+                {' '}
+                <Button onClick={this.handleClick}>SUBMIT</Button>
+            </Form>
+            </div>
         )
     }
 }
